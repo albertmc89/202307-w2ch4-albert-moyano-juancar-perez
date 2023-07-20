cdbee92b5158b3f46079849c11965b7ebd9e671d
Albert Colubi <albertmc89@gmail.com>
2023-07-21 00:45:21 +0200

    Add push method in CodersArray and add numbers to CodersArray in index

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 import CodersArray from "./CodersArray/CodersArray.js";
 
 const names = new CodersArray("albert", "joan");
+const numbers = new CodersArray(3, 8, 10, 2);
 
 console.log(names);
+
+numbers.push(22, 44);
+console.log(numbers);
